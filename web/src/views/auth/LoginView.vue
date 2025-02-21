8cb47a20023a3eee63b3aebe965bdf5f52b09109
Shayan Mathabi <shayan.mhtb@gmail.com>
2025-02-21 10:12:41 +0100

    auth views created

---
+++ web/src/views/auth/LoginView.vue
@@ -0,0 +1,9 @@
+<!-- web/src/views/auth/LoginView.vue -->
+
+<template>
+  <LoginForm />
+</template>
+
+<script setup lang="ts">
+import LoginForm from '@/components/auth/LoginForm.vue';
+</script>
---
+++ web/src/views/auth/RegisterView.vue
@@ -0,0 +1,9 @@
+<!-- web/src/views/auth/LoginView.vue -->
+
+<template>
+  <RegisterForm />
+</template>
+
+<script setup lang="ts">
+import RegisterForm from '@/components/auth/RegisterForm.vue';
+</script>
